69072de3b7291bca586707863feb4b779438a417
R Sánchez <rene.as.himself@gmail.com>
2020-04-30 15:16:28 -0500

    01 Styling the Basic Dropdown Elements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3">
+  <header
+    class="bg-gray-900 sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3"
+  >
     <div class="flex items-center justify-between px-4 py-3 sm:p-0">
       <div>
         <img class="h-8" src="img/logo-inverted.svg" alt="Workcation" />
@@ -12,14 +14,25 @@
           @click="isOpen = !isOpen"
         >
           <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
-            <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
+            <path
+              v-if="isOpen"
+              fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+            />
+            <path
+              v-if="!isOpen"
+              fill-rule="evenodd"
+              d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+            />
           </svg>
         </button>
       </div>
     </div>
 
-    <div :class="isOpen ? 'block' : 'hidden'" class="px-2 pt-2 pb-4 sm:flex sm:p-0">
+    <div
+      :class="isOpen ? 'block' : 'hidden'"
+      class="px-2 pt-2 pb-4 sm:flex sm:p-0"
+    >
       <a
         href="#"
         class="block px-2 py-2 text-white font-semibold rounded hover:bg-gray-800"
@@ -35,12 +48,19 @@
         class="block mt-1 px-2 py-2 text-white font-semibold rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
         >Messages</a
       >
+
+      <AccountDropdown />
     </div>
   </header>
 </template>
 
 <script>
+import AccountDropdown from './AccountDropdown'
+
 export default {
+  components: {
+    AccountDropdown,
+  },
   data() {
     return {
       isOpen: false,
