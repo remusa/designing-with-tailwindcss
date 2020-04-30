898f73c81f1d9678ae925d8bb46ff2fc2064a1cd
R Sánchez <rene.as.himself@gmail.com>
2020-04-30 15:58:36 -0500

    01 Structuring a Basic Card

--- src/App.vue
+++ src/App.vue
@@ -44,6 +44,8 @@
       </div>
     </div>
 
+    <PropertyCard :property="property" />
+
     <div>
       <div class="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
         <h2 class="text-xl text-gray-900">Popular destinations</h2>
@@ -67,12 +69,14 @@
 <script>
 import Navbar from './components/Navbar'
 import DestinationCard from './components/DestinationCard'
+import PropertyCard from './components/PropertyCard'
 
 export default {
   name: 'app',
   components: {
     Navbar,
     DestinationCard,
+    PropertyCard,
   },
   data() {
     return {
@@ -120,6 +124,18 @@ export default {
           imageAlt: 'Beach in Miami',
         },
       ],
+      property: {
+        imageUrl:
+          'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80',
+        imageAlt: 'Rear view of modern home with pool',
+        beds: 3,
+        baths: 2,
+        title: 'Modern home in city center',
+        priceInCents: 190000,
+        formattedPrice: '$1,900.00',
+        reviewCount: 34,
+        rating: 4,
+      },
     }
   },
 }
---
+++ src/components/PropertyCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-white border rounded-lg overflow-hidden">
+    <div class="p-6">
+      <img :src="property.imageUrl" :alt="property.imageAlt" />
+      <h4>{{ property.title }}</h4>
+      <div>{{ property.beds }} beds &bull; {{ property.baths }} baths</div>
+      <div>{{ property.formattedPrice }} / week</div>
+      <div>
+        {{ property.rating }}/5 (based on {{ property.reviewCount }} reviews)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['property'],
+}
+</script>
+
+<style></style>
